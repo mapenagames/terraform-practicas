7488b860d3060b1a9bc4d27e04d6a88745339e39
mapenagames <mapenagames@gmail.com>
2024-09-09 14:45:52 -0300

    practica-3

--- practica_3/bucket.tf
+++ practica_3/bucket.tf
@@ -1,4 +1,17 @@
 resource "aws_s3_bucket" "proveedores" {
-  bucket = "twity023-marcelo-test-bucket"
+  count  = 6
+  bucket = "proveedores-${random_string.sufijo[count.index].id}"
+  tags = {
+    Owner       = "Nazareno"
+    Environment = "Dev"
+    Office      = "proveedores"
+  }
+}
 
+resource "random_string" "sufijo" {
+  count   = 6
+  length  = 8
+  special = false
+  upper   = false
+  numeric = false
 }
