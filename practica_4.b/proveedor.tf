e23138014d4acf689256494e1d58add8b9d638d2
mapenagames <mapenagames@gmail.com>
2024-09-09 17:35:13 -0300

    practica-4.b

--- practica_4.b/proveedor.tf
+++ practica_4.b/proveedor.tf
@@ -1,10 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "4.43.0"
+      source = "hashicorp/aws"
+      # (tome cualquier version dentro del >=4.36.0 y  <5.0.0 y que no sea 4.43.0
+      version = ">=4.36.0, <5.0.0, !=4.43.0"
     }
   }
+  #required_version = "1.9.5"
+  # (tome cualquier version dentro del rango 1.9.algo)
+  required_version = "~>1.9.0"
 }
 
 provider "aws" {
