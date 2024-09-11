e341364fa059451e6fc68de06a3ffab875da0e8c
mapenagames <87348782+mapenagames@users.noreply.github.com>
2024-09-11 14:34:37 -0300

    Update ec2.tf

--- practica_6Variables/ec2.tf
+++ practica_6Variables/ec2.tf
@@ -1,1 +1,6 @@
-ami-0182f373e66f89c85
+
+resource "aws_instance" "public_instance" {
+  ami                     = "ami-0182f373e66f89c85"
+  instance_type           = "t2.micro"
+  subnet_id = aws_subnet.public_subnet.id
+}
