24a4915688212d3f49db88f3b8806640c1b57f1c
mapenagames <mapenagames@gmail.com>
2024-09-16 16:45:33 -0300

    .

--- practica_7_ForEach/ec2.tf
+++ practica_7_ForEach/ec2.tf
@@ -1,20 +1,22 @@
 variable "instancias" {
   description = "Nombre de las instancias"
-  type        = list(string)
-  default     = ["apache", "mysql", "jumserver"]
+  type        = set(string)
+  #default     = ["apache", "mysql", "jumserver"]
+  default = ["mysql", "jumserver"]
 }
 
 resource "aws_instance" "public_instance" {
+  for_each               = var.instancias
   ami                    = var.ec2_specs.ami
   instance_type          = var.ec2_specs.instance_type
   subnet_id              = aws_subnet.public_subnet.id
   key_name               = data.aws_key_pair.key.key_name
   vpc_security_group_ids = [aws_security_group.sg_public_instance.id]
   user_data              = file("scripts/userdata.sh")
-  count                  = length(var.instancias)
+
 
   tags = {
-    "Name" = var.instancias[count.index] ## var.instancias[0] la 1ra vez, [1] la 2da vez.
+    "Name" = each.value
   }
 }
 
