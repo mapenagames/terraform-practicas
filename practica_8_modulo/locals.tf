0c567755f9a61eb7cb9235b4efb57b4ca5b55199
mapenagames <mapenagames@gmail.com>
2024-09-20 12:30:57 -0300

    .

---
+++ practica_8_modulo/locals.tf
@@ -0,0 +1,13 @@
+locals {
+  sufix = "${var.tags.project}-${var.tags.env}-${var.tags.region}"
+}
+
+resource "random_string" "sufijo-s3" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+locals { ## tiene valores random dado que en aws el nombre no se puede repetir
+  s3-sufix = "${var.tags.project}-${random_string.sufijo-s3.id}"
+}
