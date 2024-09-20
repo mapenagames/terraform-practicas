06f0f795feac1dbcef06365a3c2af6c1c1ccb86f
mapenagames <87348782+mapenagames@users.noreply.github.com>
2024-09-20 15:09:23 -0300

    Update vpc.tf

--- practica_8_modulos_B/vpc.tf
+++ practica_8_modulos_B/vpc.tf
@@ -96,3 +96,16 @@ module "mybucket" {
 output "s3_arnX" {
   value = module.mybucket.s3_arn
 }
+ module "terraform_state_backend" {
+     source = "cloudposse/tfstate-backend/aws"
+     # Cloud Posse recommends pinning every module to a specific version
+     version     = "0.38.1"
+     namespace  = "ejemplo"
+     stage      = "prod"
+     name       = "terraform"
+     attributes = ["state"]
+
+     terraform_backend_config_file_path = "."
+     terraform_backend_config_file_name = "backend.tf"
+     force_destroy                      = false
+   }
