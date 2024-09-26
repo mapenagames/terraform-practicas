84e95ca2c78ab15956aa0d42c16d326175d679b5
mapenagames <87348782+mapenagames@users.noreply.github.com>
2024-09-26 16:54:27 -0300

    tfc_variable

--- practica_8_modulos/vpc.tf
+++ practica_8_modulos/vpc.tf
@@ -96,3 +96,23 @@ module "mybucket" {
 output "s3_arnX" {
   value = module.mybucket.s3_arn
 }
+
+#######################################################
+#######################################################
+resource "tfe_organization" "test" {
+  name  = "my-org-name"
+  email = "admin@company.com"
+}
+
+resource "tfe_workspace" "test" {
+  name         = "my-workspace-name"
+  organization = tfe_organization.test.name
+}
+
+resource "tfe_variable" "test" {
+  key          = "my_key_name"
+  value        = "my_value_name"
+  category     = "terraform"
+  workspace_id = tfe_workspace.test.id
+  description  = "a useful description"
+}
